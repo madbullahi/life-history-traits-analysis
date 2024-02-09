0afeb1ab5c281d1063b0f8cdfe271408376e6ac9
Muhammad <mabdullahi259@protonmail.com>
2024-02-09 13:50:40 +0000

    'commit'

--- ch6.r
+++ ch6.r
@@ -8,3 +8,56 @@ require(graphics)
 xx <- -9:9
 plot(xx, sqrt(xx), col="red")
 lines(spline(xx, sqrt(abs(xx)), n=101), col="pink")
+
+# convert to upper case
+toupper("Hello World")
+
+# is 3 greater than 2?
+
+3 > 2
+
+
+my_result <- sqrt(abs(-9 ^ 3))
+my_result
+
+
+my_int <- 12L
+my_int
+
+
+??arrange()
+
+
+update.packages()
+
+
+# Update R 
+
+UpdateR()
+
+# for exploring and visualising data
+
+library(tidyverse)
+
+# for reading Excel files
+
+library(readxl)
+
+# for descriptive statistics
+
+library(psych)
+
+# for writing data to Excel
+
+library(writexl)
+
+is.vector(1:10)
+
+
+my_number <- 8.2
+sqrt(my_number)
+
+
+is.vector(my_number) # check if my_number is a vector
+
+
